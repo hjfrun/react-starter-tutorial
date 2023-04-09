678288c60e06ecc4faafa037dccc51b548860ae3
hjfrun <hjfrun@gmail.com>
2023-04-09 10:22:36 +0800

    add project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Content from './tutorial/08-user-challenge'
+import Example from './tutorial/09-project-structure/Example'
+import Navbar from './tutorial/09-project-structure/NavBar'
 
 function App() {
   return (
     <div className='container'>
-      <Content></Content>
+      <Navbar />
+      <Example />
     </div>
   )
 }
---
+++ src/tutorial/09-project-structure/Example/index.jsx
@@ -0,0 +1,13 @@
+import FirstComponent from './FirstComponent'
+import SecondComponent from './SecondComponent'
+
+const Example = () => {
+  return (
+    <div>
+      <h1>Example</h1>
+      <FirstComponent />
+      <SecondComponent />
+    </div>
+  )
+}
+export default Example
